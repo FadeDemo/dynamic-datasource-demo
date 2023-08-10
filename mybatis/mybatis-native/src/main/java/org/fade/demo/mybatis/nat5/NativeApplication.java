f1694a966558a947b6b070bb5c45aa66db4181a4
paigeman <53284808+paigeman@users.noreply.github.com>
2023-08-10 10:40:28 +0800

    Add boot class

---
+++ mybatis/mybatis-native/src/main/java/org/fade/demo/mybatis/nat5/NativeApplication.java
@@ -0,0 +1,17 @@
+package org.fade.demo.mybatis.nat5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>启动类</p>
+ * @author fade
+ */
+@SpringBootApplication
+public class NativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NativeApplication.class, args);
+    }
+
+}
